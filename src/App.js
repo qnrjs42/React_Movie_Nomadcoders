45ef2e6b3f7c0d9030348df85c09fd7ff847b277
qnrjs42 <qnrjs42@naver.com>
2020-12-16 21:20:39 +0900

    2-2. Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
-const Food = ({ favorite }) => {
-return <h1>HELOO {favorite}</h1>;
+const Food = ({ favorite, picture }) => {
+return (
+  <>
+    <h1>HELOO {favorite}</h1>
+    <i>{picture}</i>
+  </>
+);
 };
+
+const foodILike = [
+  {
+    name: "potato",
+    image: "1",
+  },
+  {
+    name: "potato2",
+    image: "2",
+  },
+  {
+    name: "potato3",
+    image: "3",
+  },
+];
 
 const App = () => {
   return (
     <div>
       Hello
-      <Food favorite="potato" />
-      <Food favorite="sweet potato" />
-      <Food favorite="kimchi" />
-      <Food favorite="rice" />
+      {foodILike.map((dish, key) => (
+        <div key={key}>
+          <Food favorite={dish.name} picture={dish.image} />
+        </div>
+      ))}
     </div>
   );
 }
